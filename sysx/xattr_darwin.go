containerd/continuity

commit 38f54c7c6a16a557d94f08c49a8ee3eb4c2457b8

Add fchmodat system calls for darwin
Expose Fchmodat for linux and darwin through sysx package

Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

sysx/xattr_darwin.go
@@ -8,6 +8,7 @@ package sysx
 //sys  setxattr(path string, attr string, data []byte, flags int) (err error)
 //sys  removexattr(path string, attr string, options int) (err error)
 //sys  listxattr(path string, dest []byte, options int) (sz int, err error)
+//sys  Fchmodat(dirfd int, path string, mode uint32, flags int) (err error)
 
 const (
 	xattrNoFollow = 0x01

sysx/chmod_darwin.go
@@ -1,0 +1,18 @@
+package sysx
+
+const (
+	// AtSymlinkNoFollow defined from AT_SYMLINK_NOFOLLOW in <sys/fcntl.h>
+	AtSymlinkNofollow = 0x20
+)
+
+const (
+
+	// SYS_FCHMODAT defined from golang.org/sys/unix
+	SYS_FCHMODAT = 467
+)
+
+// These functions will be generated by generate.sh
+//    $ GOOS=darwin GOARCH=386 ./generate.sh chmod
+//    $ GOOS=darwin GOARCH=amd64 ./generate.sh chmod
+
+//sys  Fchmodat(dirfd int, path string, mode uint32, flags int) (err error)

sysx/chmod_darwin_386.go
@@ -1,0 +1,25 @@
+// mksyscall.pl -l32 chmod_darwin.go
+// MACHINE GENERATED BY THE COMMAND ABOVE; DO NOT EDIT
+
+package sysx
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+// THIS FILE IS GENERATED BY THE COMMAND AT THE TOP; DO NOT EDIT
+
+func Fchmodat(dirfd int, path string, mode uint32, flags int) (err error) {
+	var _p0 *byte
+	_p0, err = syscall.BytePtrFromString(path)
+	if err != nil {
+		return
+	}
+	_, _, e1 := syscall.Syscall6(SYS_FCHMODAT, uintptr(dirfd), uintptr(unsafe.Pointer(_p0)), uintptr(mode), uintptr(flags), 0, 0)
+	use(unsafe.Pointer(_p0))
+	if e1 != 0 {
+		err = errnoErr(e1)
+	}
+	return
+}

sysx/chmod_darwin_amd64.go
@@ -1,0 +1,25 @@
+// mksyscall.pl chmod_darwin.go
+// MACHINE GENERATED BY THE COMMAND ABOVE; DO NOT EDIT
+
+package sysx
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+// THIS FILE IS GENERATED BY THE COMMAND AT THE TOP; DO NOT EDIT
+
+func Fchmodat(dirfd int, path string, mode uint32, flags int) (err error) {
+	var _p0 *byte
+	_p0, err = syscall.BytePtrFromString(path)
+	if err != nil {
+		return
+	}
+	_, _, e1 := syscall.Syscall6(SYS_FCHMODAT, uintptr(dirfd), uintptr(unsafe.Pointer(_p0)), uintptr(mode), uintptr(flags), 0, 0)
+	use(unsafe.Pointer(_p0))
+	if e1 != 0 {
+		err = errnoErr(e1)
+	}
+	return
+}

sysx/chmod_linux.go
@@ -1,0 +1,12 @@
+package sysx
+
+import "syscall"
+
+const (
+	// AtSymlinkNoFollow defined from AT_SYMLINK_NOFOLLOW in /usr/include/linux/fcntl.h
+	AtSymlinkNofollow = 0x100
+)
+
+func Fchmodat(dirfd int, path string, mode uint32, flags int) error {
+	return syscall.Fchmodat(dirfd, path, mode, flags)
+}

commit d2ce1bcaeea9dee644a01097e1dfc21ce5b75632

sysx/xattr_darwin.go: rm duplicate Fchmodat def
Commit 38f54c7c6 "Add fchmodat system calls for darwin" mistakenly added
Fchmodat() for Darwin in two places.

The error is hidden as the generated files (sysx/xattr_darwin_{i386,amd64}.go
were not re-generated since.

Fixes: 38f54c7c6 "Add fchmodat system calls for darwin"
Cc: Derek McGowan <derek@mcgstyle.net>
Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

sysx/xattr_darwin.go
@@ -8,7 +8,6 @@ package sysx
 //sys  setxattr(path string, attr string, data []byte, flags int) (err error)
 //sys  removexattr(path string, attr string, options int) (err error)
 //sys  listxattr(path string, dest []byte, options int) (sz int, err error)
-//sys  Fchmodat(dirfd int, path string, mode uint32, flags int) (err error)
 
 const (
 	xattrNoFollow = 0x01