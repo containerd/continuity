643e66e4bb3e30dda0a35b6468fb5f35cde7d856
Kir Kolyshkin <kolyshkin@gmail.com>
2020-02-26 20:19:30 -0800

    Remove Windows' Readlink fork
    This reverts the part of commit 8100e750d (PR #113)
    that added the fork of Readlink() for Windows.
    
    At that time the fork was done to work around the bug
    in golang's implementation of os.Readlink() for Windows.
    
    The above bug was never reported upstream, but fortunately
    it was independently found, reported [1], and fixed [2].
    The fix made its way into go-1.13 (there is no mention of
    that in release notes, so I checked it in git history).
    
    [1] https://github.com/golang/go/issues/30463
    [2] https://go-review.googlesource.com/c/go/+/164201
    
    Signed-off-by: Kir Kolyshkin <kolyshkin@gmail.com>

--- driver/driver_windows.go
+++ driver/driver_windows.go
@@ -24,8 +24,6 @@ package driver
 
 import (
 	"os"
-
-	"github.com/containerd/continuity/sysx"
 )
 
 func (d *driver) Mknod(path string, mode os.FileMode, major, minor int) error {
@@ -40,10 +38,4 @@ func (d *driver) Mkfifo(path string, mode os.FileMode) error {
 func (d *driver) Lchmod(path string, mode os.FileMode) (err error) {
 	// TODO: Use Window's equivalent
 	return os.Chmod(path, mode)
-}
-
-// Readlink is forked in order to support Volume paths which are used
-// in container layers.
-func (d *driver) Readlink(p string) (string, error) {
-	return sysx.Readlink(p)
 }
