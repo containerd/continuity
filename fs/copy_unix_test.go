ca52b934dd01ca5e16244bbe3643a5019360d11c
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2022-03-30 16:34:37 +0900

    fs.CopyDir: support sockets and pipes
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- fs/copy_unix_test.go
+++ fs/copy_unix_test.go
@@ -22,10 +22,13 @@ package fs
 import (
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"syscall"
 	"testing"
 
 	"github.com/containerd/continuity/fs/fstest"
 	"github.com/containerd/continuity/sysx"
+	"golang.org/x/sys/unix"
 )
 
 func assertXAttr(t *testing.T, dir, xattr, xval string, xerr error) {
@@ -85,4 +88,89 @@ func TestCopyDirWithXAttrExcludes(t *testing.T) {
 		assertXAttr(t, dst, "user.test-2", "two", nil)
 		assertXAttr(t, dst, "user.test-x", "", sysx.ENODATA)
 	})
+}
+
+func TestCopyIrregular(t *testing.T) {
+	var prepared int
+	prepareSrc := func(src string) {
+		f0Pipe := filepath.Join(src, "f0.pipe")
+		if err := unix.Mkfifo(f0Pipe, 0600); err != nil {
+			t.Fatal(err)
+		}
+		prepared++
+		f1Normal := filepath.Join(src, "f1.normal")
+		if err := ioutil.WriteFile(f1Normal, []byte("content of f1.normal"), 0600); err != nil {
+			t.Fatal(err)
+		}
+		prepared++
+		f2Socket := filepath.Join(src, "f2.sock")
+		if err := unix.Mknod(f2Socket, 0600|unix.S_IFSOCK, 0); err != nil {
+			t.Fatal(err)
+		}
+		prepared++
+		f3Dev := filepath.Join(src, "f3.dev")
+		if err := unix.Mknod(f3Dev, 0600|unix.S_IFCHR, 42); err != nil {
+			t.Logf("skipping testing S_IFCHR: %v", err)
+		} else {
+			prepared++
+		}
+	}
+
+	verifyDst := func(dst string) {
+		entries, err := ioutil.ReadDir(dst)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var verified int
+		for _, f := range entries {
+			name := f.Name()
+			full := filepath.Join(dst, name)
+			fi, err := os.Stat(full)
+			if err != nil {
+				t.Fatal(err)
+			}
+			mode := fi.Mode()
+			switch name {
+			case "f0.pipe":
+				if mode&os.ModeNamedPipe != os.ModeNamedPipe {
+					t.Fatalf("unexpected mode of %s: %v", name, mode)
+				}
+			case "f1.normal":
+				b, err := ioutil.ReadFile(full)
+				if err != nil {
+					t.Fatal(err)
+				}
+				if string(b) != "content of f1.normal" {
+					t.Fatalf("unexpected content of %s: %q", name, string(b))
+				}
+			case "f2.sock":
+				if mode&os.ModeSocket != os.ModeSocket {
+					t.Fatalf("unexpected mode of %s: %v", name, mode)
+				}
+			case "f3.dev":
+				if mode&os.ModeDevice != os.ModeDevice {
+					t.Fatalf("unexpected mode of %s: %v", name, mode)
+				}
+				sys, ok := fi.Sys().(*syscall.Stat_t)
+				if !ok {
+					t.Fatalf("unexpected type: %v", fi.Sys())
+				}
+				if sys.Rdev != 42 {
+					t.Fatalf("unexpected rdev of %s: %d", name, sys.Rdev)
+				}
+			}
+			verified++
+		}
+		if verified != prepared {
+			t.Fatalf("prepared %d files, verified %d files", prepared, verified)
+		}
+	}
+
+	src := t.TempDir()
+	dst := t.TempDir()
+	prepareSrc(src)
+	if err := CopyDir(dst, src); err != nil {
+		t.Fatal(err)
+	}
+	verifyDst(dst)
 }
